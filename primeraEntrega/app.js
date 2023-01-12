135b6b86265036221c5722463c746f618f0043cd
Enzo Fernandez <enzoabel1@gmail.com>
2023-01-11 21:51:54 -0300

    fs

--- primeraEntrega/app.js
+++ primeraEntrega/app.js
@@ -2,16 +2,11 @@ const express = require("express")
 const { Router } = require("express")
 const { urlencoded } = require("body-parser")
 const app = express()
-const admin = false
+const fs = require("fs")
 
-const products = []
-const carts = [
-    { id: 1, timeStamp: '10/1/2023 20:16:58', products: [{id: 75, desc:"asda"}, {id: 12, desc:"ete"}
-] },
-    { id: 27, timeStamp: '10/1/2023 20:16:58', products: [{id: 75, desc:"asda"}, {id: 12, desc:"ete"}] },
-    { id: 14, timeStamp: '10/1/2023 20:16:58', products: ["dag", {ead: 12}] },
-    { id: 35, timeStamp: '10/1/2023 20:16:58', products: ["22", "78", "86"] }
-]
+const admin = true
+
+const products = JSON.parse(fs.readFileSync("./products.txt"))
 
 const getId = (arr) => {
     if (arr[arr.length - 1]){
@@ -19,6 +14,18 @@ const getId = (arr) => {
     } else {
         return 1
     }
+}
+const write = (path, file, replace) => {
+    const content = JSON.parse(fs.readFileSync(path))
+    if (replace) {
+        fs.writeFileSync(path, JSON.stringify(file, null, 2))
+        return
+    }
+    content.push(file)
+    fs.writeFileSync(path, JSON.stringify(content, null, 2))
+}
+const read = (path) => {
+    return JSON.parse(fs.readFileSync(path))
 }
 
 const productsRouter = Router()
@@ -32,36 +39,42 @@ app.listen(8080, ()=>{console.log("rdy")})
 
 productsRouter.get("/:id", (req, res)=>{
     const num = req.params.id
-    if (!carts.find((prod)=>prod.id == num)){
+    const products = read("./products.txt")
+    if (!products.find((prod)=>prod.id == num)){
+        res.status(404)
         res.json({error: "producto no encontrado"})
         return
     }
-    res.json(carts.find((prod)=>prod.id == num))
+    res.json(products.find((prod)=>prod.id == num))
 })
 productsRouter.post("/", (req, res)=>{
     if (admin){
         const productToAdd = req.body
-        productToAdd.id = carts.length + 1
-        carts.push(productToAdd)
+        productToAdd.id = read("./products.txt").length + 1
+        write("./products.txt", productToAdd)
         res.json(productToAdd)
     } else {
         res.status(403)
         res.send(`Error 403: ${req.method} "${req.path}" no autorizado`)
     }
 })
-productsRouter.put("/", (req, res)=>{
+productsRouter.put("/:id", (req, res)=>{
     if (admin){
         const productUpdate = req.body
         const num = req.params.id
+        const products = read("./products.txt")
         if (!products.find((prod)=>prod.id == num)){
+            res.status(404)
             res.json({error: "producto no encontrado"})
             return
         }
         const index = products.findIndex((prod)=>prod.id == num)
-        products[index].price = productUpdate.price
-        products[index].title = productUpdate.title
-        products[index].thumbnail = productUpdate.thumbnail
-        res.json({actualizado: products[index]})
+        if (productUpdate.price) products[index].price = productUpdate.price
+        if (productUpdate.title) products[index].title = productUpdate.title
+        if (productUpdate.thumbnail) products[index].thumbnail = productUpdate.thumbnail
+        const updated = products[index]
+        write("./products.txt", products, true)
+        res.json({actualizado: updated})
     } else {
         res.status(403)
         res.send(`Error 403: ${req.method} "${req.path}" no autorizado`)
@@ -70,13 +83,15 @@ productsRouter.put("/", (req, res)=>{
 productsRouter.delete("/:id", (req, res)=>{
     if (admin){
         const num = req.params.id
-        if (!carts.find((prod)=>prod.id == num)){
+        const products = read("./products.txt")
+        if (!products.find((prod)=>prod.id == num)){
             res.json({error: "producto no encontrado"})
             return
         }
-        const index = carts.findIndex((prod)=>prod.id == num)
-        const deletedProduct = carts[index]
-        carts.splice(index, 1)
+        const index = products.findIndex((prod)=>prod.id == num)
+        const deletedProduct = products[index]
+        products.splice(index, 1)
+        write("./products.txt", products, true)
         res.json({deleted: deletedProduct})
     } else {
         res.status(403)
@@ -93,40 +108,40 @@ cartRouter.get("/", (_, res)=>{
 cartRouter.post("/", (req, res)=>{
     const time = Date.now()
     const cartProducts = req.body
-    const newCart = {id: getId(carts), timeStamp: new Date(time).toLocaleString(), products: cartProducts}
-    carts.push(newCart)
+    const newCart = {id: getId(products), timeStamp: new Date(time).toLocaleString(), products: cartProducts}
+    products.push(newCart)
     res.json({ cartID: newCart.id })
 })
 cartRouter.delete("/:id", (req, res)=>{
     const num = req.params.id
-    if (!carts.find((cart)=>cart.id == num)){
+    if (!products.find((cart)=>cart.id == num)){
         res.json({error: "carrito no encontrado"})
         return
     }
-    const index = carts.findIndex((cart)=>cart.id == num)
-    carts.splice(index, 1)
+    const index = products.findIndex((cart)=>cart.id == num)
+    products.splice(index, 1)
     res.send(`Carrito con id:${num} eliminado.`)
 })
 cartRouter.get("/:id/productos", (req, res)=>{
     const num = req.params.id
-    const index = carts.findIndex((cart)=>cart.id == num)
-    res.json(carts[index].products)
+    const index = products.findIndex((cart)=>cart.id == num)
+    res.json(products[index].products)
 })
 cartRouter.post("/:id/productos", (req, res)=>{
     const num = req.params.id
     const productToAdd = req.body
-    const index = carts.findIndex((cart)=>cart.id == num)
-    carts[index].products.push(productToAdd)
+    const index = products.findIndex((cart)=>cart.id == num)
+    products[index].products.push(productToAdd)
     res.send(`${productToAdd} añadido`)
 })
 cartRouter.delete("/:id/productos/:id_prod", (req, res)=>{
     const cartID = req.params.id
     const prodID = req.params.id_prod
-    const cartIndex = carts.findIndex((cart)=>cart.id == cartID)
-    const prodIndex = carts[cartIndex].products.findIndex((prod)=>prod.id == prodID)
-    const eliminatedProduct = carts[cartIndex].products[prodIndex]
-    console.log(carts)
-    carts[cartIndex].products.splice(prodIndex, 1)
+    const cartIndex = products.findIndex((cart)=>cart.id == cartID)
+    const prodIndex = products[cartIndex].products.findIndex((prod)=>prod.id == prodID)
+    const eliminatedProduct = products[cartIndex].products[prodIndex]
+    console.log(products)
+    products[cartIndex].products.splice(prodIndex, 1)
     res.json({eliminado: eliminatedProduct})
-    console.log(carts)
+    console.log(products)
 })
