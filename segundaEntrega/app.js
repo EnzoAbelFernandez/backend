81baa65c2b9ef9290a8b57d11d730ab4b7ccd180
Enzo Fernandez <enzoabel1@gmail.com>
2023-02-07 21:46:57 -0300

    2

--- segundaEntrega/app.js
+++ segundaEntrega/app.js
@@ -1,39 +1,3 @@
-/*class Productos {
-    constructor(nombre){
-        fs.writeFileSync(`./${nombre}.txt`, "[]", ()=>{console.log("listo")})
-        path = `./${nombre}.txt`
-    }
-    save(producto){
-        let doc = fs.readFileSync(`./zapas.txt`, "utf-8", ()=>{console.log("ok")})       
-        producto.id = (JSON.parse(doc).length) + 1
-        let id = producto.id
-        doc = JSON.parse(doc)
-        doc.push(producto)
-        fs.writeFileSync("./zapas.txt", JSON.stringify(doc), ()=>id)
-    }
-    getById(id){
-        let doc = fs.readFileSync(`./zapas.txt`, "utf-8", ()=>{console.log("ok")})
-        doc = JSON.parse(doc)
-        let producto = doc.find(prod=>prod.id === id)
-        return producto
-    }
-    getAll(){
-        let doc = fs.readFileSync(`./zapas.txt`, "utf-8", ()=>{console.log("ok")})
-        doc = JSON.parse(doc)
-        return doc
-    }
-    deleteById(id){
-        let doc = fs.readFileSync(`./zapas.txt`, "utf-8", ()=>{console.log("ok")})
-        doc = JSON.parse(doc)
-        let producto = doc.find(prod=>prod.id === id)
-        doc.splice(producto.id-1, 1)
-        fs.writeFileSync("./zapas.txt", JSON.stringify(doc), ()=>{console.log("borrado");})
-    }
-    deleteAll(){
-        fs.writeFileSync(`./zapas.txt`, "[]", ()=>{console.log("listo")})
-    }
-} */
-
 const mongoose = require('mongoose')
 const {model, Schema} = mongoose
 
@@ -43,11 +7,12 @@ async function main() {
 
 main().catch(err => console.log(err))
 
+const productsSchema = new Schema(
+    {title: String, price: Number}, {timestamps: true}
+)
+
 class Products{
     constructor(name){
-        const productsSchema = new Schema(
-            {title: String, price: Number}, {timestamps: true}
-        )
         const Product = mongoose.model('product', productsSchema)
     }
     save(title, price){
